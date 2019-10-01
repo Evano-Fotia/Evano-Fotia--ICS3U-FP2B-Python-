77ebccf466f57dfb1a5c2c332640041edf92584b
Ubuntu <ubuntu@ip-172-31-28-26.ec2.internal>
2019-10-01 23:39:12 +0000

    Initial commit

---
+++ Ellipe.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Created by: Evano Fotia
+# Created on: Sep 2019
+# This code is to find the area of an ellipse
+
+import math
+
+# input
+    
+    A = int(input(" enter A line here (cm): "))
+    B = int(input(" enter B line here"))
+    
+    
+    # process
+    
+    π × A × B = area
+    
+    #output
+    print("")
+    print("The area of your ellipse ", area_)
+    print("(", side A, " + ", side b, " = ", area_, ")")
+    
+    
+
+if __name__ == "__main__":
+    main()
